6d8f45eb61b4197d90d56635f1fa1bfeda19884f
Beatriz Jungers Mattos <bmattos@outlook.de>
2023-06-01 17:40:43 -0300

    add new route createCar

--- backend/src/router.js
+++ backend/src/router.js
@@ -3,6 +3,10 @@ const carController = require('./controllers/carController');
 
 const router = express.Router();
 
+router.post('/cars', carController.createCar);
 router.get('/cars', carController.readAllCars);
+//router.get('/cars/:id', carController.readCarById);
+//router.put('/cars/:id', carController.updateCarById);
+//router.delete('/cars/:id', carController.deleteCarById);
 
 module.exports = router;
